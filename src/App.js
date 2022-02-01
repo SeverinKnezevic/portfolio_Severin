3cc3dcc329c67856f83f423548c2a45f02fe3c1d
Severin <knezevicseverin@gmail.com>
2022-02-01 15:09:48 +0100

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h2>Portfolio in progress...</h2>
+    // <h2>Portfolio in progress...</h2>
+    <Navbar />
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+//  REACT FONTAWESOME IMPORT
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+    <div>
+        
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+          <div className="container">
+          <a className="navbar-brand" href="#">SEVERIN</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{color: "fff"}}/>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Project</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
