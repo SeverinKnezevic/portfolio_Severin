f74bb23d019cd603cc23c6f70b402c95d8d0672f
Severin <knezevicseverin@gmail.com>
2022-02-12 16:23:53 +0100

    12/2/2022

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,50 +1,45 @@
 import React, {useState} from 'react';
 import emailjs from '@emailjs/browser';
-import {useForm} from 'react-hook-form';
+import {set, useForm} from 'react-hook-form';
+
+
 
 const Contacts = () => {
 
-  const [successMessage, setSuccessMessage] = useState("");
-  const { register, handleSubmit, formState: { errors } } = useForm();
-
-  const serviceID = "service_ID";
-  const templateID = "template_ID" ;
-  const userID = "user_ZSmVBLO7vuIS8oRwcL7Zv";
-
- 
-  const onSubmit = (data, r) => {
-    sendEmail(
-      serviceID,
-      templateID,
-      {
-        name: data.name,
-        email: data.email,
-        subject: data.subject,
-        message: data.message
-      },
-      userID
+  const Result = () => {
+    return (
+      <span><i>Form sent successfully!</i> </span>
     )
-    r.target.reset();
   }
 
-  const sendEmail = (serviceID, templateID, variables, userID) => {
-    emailjs.send(serviceID, templateID, variables, userID)
-      .then(() => {
-        setSuccessMessage("Form sent successfully!");
-      }).catch(err => console.error(`Something went wrong ${err}`));
-  }
+  const [result, showResult] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_ID", "template_ID", e.target, "user_ZSmVBLO7vuIS8oRwcL7Zv")
+      .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        }
+      );
+    e.target.reset();
+    showResult(true);
+  };
+
+
 
   return (
     <div id="contacts" className='contacts'>
       <div className="text-center">
         <h1>Contact Me</h1>
         <p> Please fill out the form and I'll contact you as soon as possible.</p>
-        <span className="success-message">{successMessage}</span>
+        <span className="success-message"> {result ? <Result /> : null} </span>
       </div>
       
       <div className="container">
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={sendEmail} autoComplete="off">
 
           <div className="row">
             <div className="col-md-6 col-xs-12">
@@ -57,19 +52,13 @@ const Contacts = () => {
                 className="form-control"
                 placeholder="Name"
                 name="name"
-                  // ref={
-                  //   register({
-                  //     required: "Please enter your name",
-                  //     maxLength: {
-                  //       value: 20,
-                  //       mess: "Please enter a name with fewer  than 20 characters"
-                  //     }
-                  //   })
-                  // }
+                required
               />
               </div>
+
               <span className="error-message">
-                { errors.name && errors.name.message }
+                {/* { errors.name && errors.name.message } */}
+                
               </span>
 
               {/* INPUT EMAIL */}
@@ -79,18 +68,8 @@ const Contacts = () => {
                 className='form-control'
                 placeholder='Email'
                 name='email'
-                // ref={
-                //   register({
-                //     required: "Please provide you email.",
-                //     pattern: {
-                //       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                //       message: "invalid Email"
-                //     }
-                //   })}
+                required
               />
-                {/* <span className="error-message">
-                {errors.email && errors.email.message}
-                </span> */}
 
               {/* SUBJECT EMAIL */}
               <input
@@ -99,11 +78,7 @@ const Contacts = () => {
                 className='form-control'
                 placeholder='Subject'
                 name='subject'
-                // ref={
-                //   register({
-                //     required: "OOPS, you forget to add the subject.",
-                //   })
-                // }
+                required
               />
             </div>
               {/* <span className="error-message">
@@ -118,11 +93,7 @@ const Contacts = () => {
                   className='form-control'
                   placeholder='Message'
                   name='message'
-                    // ref={
-                    //   register({
-                    //     required: "Please message...",
-                    //   })
-                    // }
+                  required
               ></textarea>
               {/* <span className="error-message">
                 {errors.message && errors.message.message}
